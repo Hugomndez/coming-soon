6a5f67bbe3638afe1861a7a40c278e5f2fe9cdfc
Hugo Méndez <hello@hugomendez.dev>
2024-11-05 01:10:04 -0500

    build: upgrate to next15

--- src/app/_components/SubscriptionForm/use-subscription-form.hook.ts
+++ src/app/_components/SubscriptionForm/use-subscription-form.hook.ts
@@ -1,7 +1,6 @@
 import type { FormEventTarget, SubscriptionState } from '@/entities/models/subscription';
 import type { ChangeEvent, FocusEvent } from 'react';
-import { useCallback, useEffect } from 'react';
-import { useFormState } from 'react-dom';
+import { useActionState, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { useImmer } from 'use-immer';
 import subscriptionAction from './subscription-form.action';
@@ -14,7 +13,7 @@ const initialState: SubscriptionState = {
 };
 
 export default function useSubscriptionForm() {
-  const [subscriptionState, formAction] = useFormState(subscriptionAction, initialState);
+  const [subscriptionState, formAction] = useActionState(subscriptionAction, initialState);
   const [formState, setFormState] = useImmer<SubscriptionState>(subscriptionState);
 
   useEffect(() => {
@@ -26,26 +25,20 @@ export default function useSubscriptionForm() {
     (handleStatus[subscriptionState.status] || handleStatus.default)();
   }, [subscriptionState, setFormState]);
 
-  const handleBlur = useCallback(
-    (event: FocusEvent<HTMLInputElement>) => {
-      const { name } = event.target as FormEventTarget;
-      setFormState((draft) => void (draft.form.fieldBlurs[name] = true));
-    },
-    [setFormState]
-  );
+  const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
+    const { name } = event.target as FormEventTarget;
+    setFormState((draft) => void (draft.form.fieldBlurs[name] = true));
+  };
 
-  const handleChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target as FormEventTarget;
-      setFormState((draft) => {
-        draft.form.data[name] = value;
-        const { fieldErrors, status } = validateForm(draft.form.data);
-        draft.form.fieldErrors = fieldErrors;
-        draft.status = status;
-      });
-    },
-    [setFormState]
-  );
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target as FormEventTarget;
+    setFormState((draft) => {
+      draft.form.data[name] = value;
+      const { fieldErrors, status } = validateForm(draft.form.data);
+      draft.form.fieldErrors = fieldErrors;
+      draft.status = status;
+    });
+  };
 
   return {
     formState,
